c10ed6c5d845de2aafc50e59cd199860d21fad46
Aster0N <nikvas131@gmail.com>
2023-07-03 12:41:48 +0300

    Added first text block and added another link to login

---
+++ src/components/UI/button/MyButton.jsx
@@ -0,0 +1,16 @@
+import classes from './MyButton.module.css'
+
+const MyButton = ({ children, ...props }) => {
+	return (
+		<>
+			<button
+				{...props}
+				className={[classes.myButton, props?.className].join(' ')}
+			>
+				{children}
+			</button>
+		</>
+	)
+}
+
+export default MyButton
--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,5 +1,7 @@
 import AccentTitle from '@/components/UI/accentTitle/AccentTitle'
 import SmallTextBlock from '@/components/UI/smallTextBlock/SmallTextBlock'
+import { Link } from 'react-router-dom'
+import MyButton from '../../components/UI/button/MyButton'
 import classes from './Home.module.css'
 
 const Home = () => {
@@ -15,6 +17,11 @@ const Home = () => {
 					<AccentTitle>RFC</AccentTitle>
 					<SmallTextBlock className={classes.SmallTextBlockWithMargin}>
 						Is an online cinema that offers a wide range of movies and TV shows for its audience.
+						<Link to="/login">
+							<MyButton
+								className={classes.buttonInTextBlock}
+							>explore</MyButton>
+						</Link>
 					</SmallTextBlock>
 				</div>
 			</div>
