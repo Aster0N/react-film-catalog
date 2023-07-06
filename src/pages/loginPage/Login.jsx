6383cc3a3c0f90f896c0082b4420a1ed653b9205
Aster0N <nikvas131@gmail.com>
2023-07-06 15:42:44 +0300

    Styled link to the home page in 'login' page. Made page always full-height

--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -1,9 +1,17 @@
+import shortLeftArrow from '@/assets/img/short-left-arrow.svg'
 import { Link } from "react-router-dom"
+import classes from './Login.module.css'
+
 const Login = () => {
 	return (
-		<>
-			<Link to="..">back</Link>
-		</>
+		<div className="_page">
+			<div className="_wrapper">
+				<Link to=".." className={classes.backLink}>
+					<img src={shortLeftArrow} alt="go back" />
+					<span className={classes.backLinkCaption}>go back</span>
+				</Link>
+			</div>
+		</div>
 	)
 }
 
