4dcff67fe91b219e0f1cf4cc9a8eb5dc73bdfe98
Aster0N <nikvas131@gmail.com>
2023-07-13 12:04:05 +0300

    Added dividers between nav-links. Added location pathname checking to display different content in navbar

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -1,16 +1,23 @@
 import logo from "@/assets/img/dark-logo.svg"
-import { Link } from 'react-router-dom'
+import NavMenu from '@/components/navMenu/NavMenu.jsx'
+import { Link, useLocation } from 'react-router-dom'
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
+	const location = useLocation()
+
 	return (
 		<div className={classes.navbar}>
 			<div
 				className={[classes.navbarContent, '_wrapper'].join(' ')}
 			>
-				<Link to="/"><img src={logo} alt="logo" /></Link>
+				{(location.pathname !== '/')
+					? <NavMenu />
+					: <Link to="/"><img src={logo} alt="logo" /></Link>
+				}
 				<div className={classes.links}>
 					<Link to="/catalog" className={classes.link}>catalog</Link>
+					<span className={classes.linkDivider}>/</span>
 					<Link to="/login" className={classes.link}>login</Link>
 				</div>
 			</div>
---
+++ src/components/navMenu/NavMenu.jsx
@@ -0,0 +1,7 @@
+const NavMenu = () => {
+	return (
+		<div>menu</div>
+	)
+}
+
+export default NavMenu
