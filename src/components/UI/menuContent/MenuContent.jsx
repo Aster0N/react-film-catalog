c3d490141f5526b715c6743cf15424d1057a26bd
Aster0N <nikvas131@gmail.com>
2023-07-14 11:15:43 +0300

    Added menu content

--- src/components/UI/menuContent/MenuContent.jsx
+++ src/components/UI/menuContent/MenuContent.jsx
@@ -1,6 +1,22 @@
+import { Link } from 'react-router-dom'
+import classes from './MenuContent.module.css'
+
 const MenuContent = ({ isOpen }) => {
 	return (
-		<div>menu {isOpen ? 'open' : 'close'}</div>
+		<div
+			className={
+				[classes.menuWrapper,
+				isOpen ? classes.menuOpen : ''
+				].join(' ')}
+		>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/online-cinema">Online cinema</Link></li>
+				<li><Link to="/catalog">Catalog</Link></li>
+				<li><Link to="/favorites">Favorites</Link></li>
+				<li><Link to="/saved">Saved</Link></li>
+			</ul>
+		</div>
 	)
 }
 
