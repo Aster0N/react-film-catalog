744966e007765e73b12df9fea915da23d531a766
Aster0N <nikvas131@gmail.com>
2023-07-25 17:22:03 +0300

    Styled burger menu component

--- src/components/UI/burger/Burger.jsx
+++ src/components/UI/burger/Burger.jsx
@@ -1,6 +1,22 @@
+import classes from './Burger.module.css'
+
 const Burger = ({ showMenu }) => {
 	return (
-		<button onClick={showMenu}>open</button>
+		<button
+			className={classes.burger}
+			onClick={showMenu}
+		>
+			<div className={classes.lines}>
+				<div className={[classes.linesWrapper, classes.linesWrapper1].join(' ')}>
+					<span className={classes.line}></span>
+					<span className={classes.line}></span>
+				</div>
+				<div className={[classes.linesWrapper, classes.linesWrapper2].join(' ')}>
+					<span className={classes.line}></span>
+					<span className={classes.line}></span>
+				</div>
+			</div>
+		</button>
 	)
 }
 
