f247400b492aa7ab21d188d7bfdbe52d4dcb4f10
Aster0N <nikvas131@gmail.com>
2023-09-08 13:53:01 +0300

    Styled movie card

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,16 +1,21 @@
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import classes from './MovieCard.module.css'
 
 const MovieCard = ({ movieData }) => {
-	const navigate = useNavigate()
-
 	return (
-		<div
-			className={classes.card}
-			onClick={() => navigate(`/online-cinema/movie/${movieData.id}`)}
-		>
-			{movieData.title}
-		</div>
+		<Link to={`/online-cinema/movie/${movieData.id}`}>
+			<div
+				className={classes.card}
+				style={{
+					backgroundImage: `url(${movieData.backdrop})`,
+					backgroundPosition: 'center',
+					backgroundSize: 'cover',
+					backgroundRepeat: 'no-repeat'
+				}}
+			>
+				<span className={classes.movieTitle}>{movieData.title}</span>
+			</div>
+		</Link>
 	)
 }
 
