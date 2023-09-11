ffda9c1f97e187a3a6e0d2a7813a3da30e87a23d
Aster0N <nikvas131@gmail.com>
2023-09-11 15:22:46 +0300

    Setted limit of expansions of movie preview list

--- src/pages/onlineCinema/OnlineCinema.jsx
+++ src/pages/onlineCinema/OnlineCinema.jsx
@@ -1,3 +1,4 @@
+import MyButton from '@/components/UI/button/MyButton'
 import MovieCard from '@/components/movieCard/MovieCard'
 import CinemaService from '@/services/CinemaService'
 import { useEffect, useState } from 'react'
@@ -5,6 +6,9 @@ import classes from './OnlineCinema.module.css'
 
 const OnlineCinema = () => {
 	const [movieListPreview, setMovieListPreview] = useState([])
+	const [hideShowMoreBtn, setHideShowMoreBtn] = useState(false)
+	const [expansionsCount, setExpansionsCount] = useState(0)
+	const listExpansionsLimit = 2
 
 	const getListPreview = async () => {
 		const response = await CinemaService.getMovieListPreview(10)
@@ -12,6 +16,15 @@ const OnlineCinema = () => {
 	}
 
 	const showMore = async () => {
+		if (expansionsCount == listExpansionsLimit) return
+
+		setExpansionsCount(prev => {
+			if (prev + 1 == listExpansionsLimit) {
+				setHideShowMoreBtn(true)
+			}
+			return prev + 1
+		})
+
 		const response = await CinemaService.expandMovieListPreview(10)
 		setMovieListPreview(prev => {
 			return [
@@ -19,6 +32,10 @@ const OnlineCinema = () => {
 				...response
 			]
 		})
+	}
+
+	const seeAll = () => {
+		console.log('see all')
 	}
 
 	useEffect(() => {
@@ -36,9 +53,10 @@ const OnlineCinema = () => {
 						/>
 					)}
 				</div>
-				<button
-					onClick={() => showMore()}
-				>see more</button>
+				{hideShowMoreBtn
+					? <MyButton onClick={() => seeAll()}>see all</MyButton>
+					: <MyButton onClick={() => showMore()}>show more</MyButton>
+				}
 			</div>
 		</div>
 	)
