732eb8bf5d067b0e61a0b700f4cbdad0c05fd23e
Aster0N <nikvas131@gmail.com>
2023-09-11 14:04:57 +0300

    Created a preview list of movies to avoid full movie list loading on first page load. Added an ability to expand this preview list

--- src/pages/onlineCinema/OnlineCinema.jsx
+++ src/pages/onlineCinema/OnlineCinema.jsx
@@ -4,28 +4,41 @@ import { useEffect, useState } from 'react'
 import classes from './OnlineCinema.module.css'
 
 const OnlineCinema = () => {
-	const [movieList, setMovieList] = useState([])
+	const [movieListPreview, setMovieListPreview] = useState([])
 
-	const getMoviesData = async () => {
-		const response = await CinemaService.getMovieList()
-		setMovieList(response)
+	const getListPreview = async () => {
+		const response = await CinemaService.getMovieListPreview(10)
+		setMovieListPreview(response)
+	}
+
+	const showMore = async () => {
+		const response = await CinemaService.expandMovieListPreview(10)
+		setMovieListPreview(prev => {
+			return [
+				...prev,
+				...response
+			]
+		})
 	}
 
 	useEffect(() => {
-		getMoviesData()
+		getListPreview()
 	}, [])
 
 	return (
 		<div className="_page">
 			<div className="_wrapper">
 				<div className={classes.movieList}>
-					{movieList.map(movie =>
+					{movieListPreview.map(movie =>
 						<MovieCard
 							key={movie.id}
 							movieData={movie}
 						/>
 					)}
 				</div>
+				<button
+					onClick={() => showMore()}
+				>see more</button>
 			</div>
 		</div>
 	)
--- src/services/CinemaService.js
+++ src/services/CinemaService.js
@@ -1,11 +1,56 @@
 import { db } from '@/config/firebase.js'
-import { collection, doc, getDoc, getDocs } from "firebase/firestore"
+import { collection, doc, getDoc, getDocs, limit, query, startAfter } from "firebase/firestore"
+
+let previewDocsLimit = 0
 
 export default class CinemaService {
-	static async getMovieList() {
+	static async getFullMovieList() {
 		const moviesCollectionRef = collection(db, "movies")
 		try {
 			const data = await getDocs(moviesCollectionRef)
+			const filteredData = data.docs.map(doc => ({
+				...doc.data(),
+				id: doc.id
+			}))
+			return filteredData
+		} catch (err) {
+			console.error(err)
+		}
+	}
+
+	static async getMovieListPreview(docsLimit) {
+		previewDocsLimit = docsLimit
+
+		const moviesCollectionRef = collection(db, "movies")
+		try {
+			const previewList = query(moviesCollectionRef, limit(docsLimit))
+			const data = await getDocs(previewList)
+
+			const filteredData = data.docs.map(doc => ({
+				...doc.data(),
+				id: doc.id
+			}))
+			return filteredData
+		} catch (err) {
+			console.error(err)
+		}
+	}
+
+	static async expandMovieListPreview(expandedDocsLimit) {
+		const moviesCollectionRef = collection(db, "movies")
+
+		const prevList = query(moviesCollectionRef, limit(previewDocsLimit))
+		const prevListSnapshot = await getDocs(prevList)
+		const lastVisibleDoc = prevListSnapshot.docs[prevListSnapshot.docs.length - 1]
+
+		try {
+			const nextDocs = query(
+				moviesCollectionRef, startAfter(lastVisibleDoc), limit(expandedDocsLimit)
+			)
+			const data = await getDocs(nextDocs)
+
+			previewDocsLimit += expandedDocsLimit
+
 			const filteredData = data.docs.map(doc => ({
 				...doc.data(),
 				id: doc.id
