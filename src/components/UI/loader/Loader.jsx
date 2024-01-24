02a261926267269e8db19f2742b63a85a2a507c5
Aster0N <nikvas131@gmail.com>
2024-01-24 15:03:20 +0300

    Changed loader styles

--- src/components/UI/loader/Loader.jsx
+++ src/components/UI/loader/Loader.jsx
@@ -3,8 +3,9 @@ import classes from './Loader.module.css'
 const Loader = () => {
 	return (
 		<div className={classes.loaderWrapper}>
+			<span className={classes.loaderStar}></span>
 			<span className={classes.loaderCaption}>loading</span>
-			<div className={classes.loaderLine}></div>
+			<span className={classes.loaderStar}></span>
 		</div>
 	)
 }
